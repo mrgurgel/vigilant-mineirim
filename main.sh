10c4f983ad325f8f177c5a1d1388eef48fd700a8
Márcio Gurgel <marcio.rga@gmail.com>
2021-05-06 00:23:20 -0300

    Initial (and functional) script

--- main.sh
+++ main.sh
@@ -7,7 +7,6 @@ destination_path=${INPUT_DESTINATION_PATH}
 function do_move_files() {
   echo "Transferring files older than ${age_in_days_to_be_moved} days, from: ${origin_path} to: ${destination_path}"
   mkdir -p "${destination_path}"
-  ls -lsah /data/files/Fundos
   # shellcheck disable=SC2086
   find ${origin_path} -type d -ctime +"${age_in_days_to_be_moved}" -exec mv {} "${destination_path}" \;
   echo "Files moved in $(date)"
